4d2203b571e24283bb703c01a00b0cd29346d198
arrrlekino <zebrasteel@gmail.com>
2021-09-20 19:29:15 +0200

    delete migrates admin/role add gem devise

--- db/migrate/20220000000010_modify_roles.rb
+++
@@ -1,9 +0,0 @@
-class ModifyRoles < ActiveRecord::Migration[6.0]
-  def change
-    add_index :roles, :alias, unique: true
-      Role.create :alias => "manageadmin", :title => "Manage admin"
-      Role.create :alias => "manageauthor", :title => "Manage author"
-      Role.create :alias => "managebook", :group => "Manage book"
-      Role.create :alias => "managetopic", :group => "Manage topic"
-  end
-end
